a62d12915409e3f167ecc2de093dd6da2b1cdaa4
anthonysong <sanhong113@gmail.com>
2026-02-12 15:09:03 -0500

    removed uneccessary code.

--- scripts/20260207_Inverted_MUSE_BIT2HE_crypts_lieberkuhn_newHE_data/run_pretrain.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-CUDA_VISIBLE_DEVICES=0 python3 /home/durrlab/Desktop/Anthony/UGVSM/UVCGANv2_vHE/scripts/20260207_Inverted_MUSE_BIT2HE_crypts_lieberkuhn_newHE_data/pretrain.py \
-  --root_data_path /home/durrlab/Desktop/Anthony/data/duodenum_crypts_lieberkuhn_MUSE_BIT \
-  --batch-size 4
